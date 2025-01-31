c86c808849935521b7d61ee22a610bb31ad53297
Ashar Islam <55947821+asharislam@users.noreply.github.com>
2025-01-31 07:32:48 +0600

    Add files via upload

---
+++ 1703A_YES_or_YES_codeforces_solution_asharislam.py
@@ -0,0 +1,14 @@
+# 1703A	YES or YES? using python by Ashar Islam
+
+n = int(input())
+for _ in range(n):
+    s = input().upper()
+    if s == "YES":
+        print("YES")
+    else:
+        print("NO")
+
+
+# comprehension system
+# for _ in range(int(input())):
+#     print("YES" if input().upper()=="YES" else "NO")
