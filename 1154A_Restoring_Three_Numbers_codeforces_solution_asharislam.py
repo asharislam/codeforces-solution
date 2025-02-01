d7fb0b4ec734a854aa3fb62803dd657adf3fba04
Ashar Islam <55947821+asharislam@users.noreply.github.com>
2025-02-01 06:13:21 +0600

    Add files via upload

---
+++ 1154A_Restoring_Three_Numbers_codeforces_solution_asharislam.py
@@ -0,0 +1,26 @@
+# 1154A	Restoring Three Numbers using python by Ashar Islam
+
+# s = sorted(list(map(int, input().split())))
+
+# x = s[0]
+# y = s[1]
+# z = s[2]
+# k = s[3]
+#
+# a = k-z
+# b = k-y
+# c = k-x
+# print(b, a, c)
+
+
+
+s = sorted(list(map(int, input().split())))
+k = s[3]
+a = k-s[2]
+b = k-s[1]
+c = k-s[0]
+print(b, a, c)
+
+
+s = sorted(list(map(int, input().split())))
+print(s[3]-s[1], s[3]-s[2], s[3]-s[0])
