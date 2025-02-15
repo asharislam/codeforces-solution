0437f9bb50dce40f0d3690ab20891488b35d9964
Ashar Islam <55947821+asharislam@users.noreply.github.com>
2025-02-16 00:34:00 +0600

    Update 1791A_Codeforces_Checking_codeforces_solution_asharislam.py

--- 1791A_Codeforces_Checking_codeforces_solution_asharislam.py
+++ 1791A_Codeforces_Checking_codeforces_solution_asharislam.py
@@ -1,19 +1,19 @@
 # 1791A	Codeforces Checking using python by Ashar Islam
 
-n = int(input())
-s = "codeforces"
-for _ in range(n):
-  m = input()
-     if m not in s:
-       print("NO")
-     else:
-       print("YES")
-
-
 # n = int(input())
 # s = "codeforces"
-#
 # for _ in range(n):
-#     m = input().strip()
-#     print("YES" if m in s else "NO")
+#   m = input()
+#      if m not in s:
+#        print("NO")
+#      else:
+#        print("YES")
+
+
+n = int(input())
+s = "codeforces"
+
+for _ in range(n):
+    m = input().strip()
+    print("YES" if m in s else "NO")
 
