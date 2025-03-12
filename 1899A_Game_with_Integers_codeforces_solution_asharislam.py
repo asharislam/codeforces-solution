27768229bcee59cffa0a53b76d2e978365fe2c73
Ashar Islam <55947821+asharislam@users.noreply.github.com>
2025-03-13 00:42:14 +0600

    Add files via upload

---
+++ 1899A_Game_with_Integers_codeforces_solution_asharislam.py
@@ -0,0 +1,9 @@
+# 1899A	Game with Integers using python by Ashar Islam
+
+n = int(input())
+for _ in range(n):
+    s = int(input())
+    if s%3 == 0:
+        print("Second")
+    else:
+        print("First")
