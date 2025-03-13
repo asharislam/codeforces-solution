139e613ccc7b6ba4037c3fbd370ad0cfcd5b5fce
Ashar Islam <55947821+asharislam@users.noreply.github.com>
2025-03-14 05:22:27 +0600

    Add files via upload

---
+++ 1722A_Spell_Check_codeforces_solution_asharislam.py
@@ -0,0 +1,12 @@
+# 1722A	Spell Check using python by Ashar Islam
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    if n == 5 and sorted(s) == sorted("Timur"):
+        print("YES")
+    else:
+        print("NO")
+
+
